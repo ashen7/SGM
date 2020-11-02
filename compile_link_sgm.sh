5047a80d40d783a01ab45e698eef07c73b5131d6
ashen7 <986300260@qq.com>
2020-11-02 11:04:24 +0800

    更新

--- compile_link_sgm.sh
+++ compile_link_sgm.sh
@@ -1,4 +1,4 @@
-g++ main.cpp semi_global_matching.cpp sgm_util.cpp -std=gnu++11 \
+g++ main.cpp semi_global_matching.cpp sgm_util.cpp -std=gnu++11 -o sgm_stereo \
     -I /home/yipeng/thirdlib/glog/include -I /home/yipeng/thirdlib/gflags/include -I /home/yipeng/thirdlib/opencv/include/ \
     -L /home/yipeng/thirdlib/glog/lib -L /home/yipeng/thirdlib/gflags/lib -L /home/yipeng/thirdlib/opencv/lib/ \
     -lglog -lgflags -lopencv_highgui -lopencv_core -lopencv_imgproc -lopencv_imgcodecs
